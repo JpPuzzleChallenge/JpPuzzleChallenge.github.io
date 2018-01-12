2432722ebe274a170ac48a78140a33dd62370fdd
JpPuzzleChallenge <31852167+JpPuzzleChallenge@users.noreply.github.com>
2018-01-12 11:43:29 +0800

    Updated!

---
+++ codechecker1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php
+if ($_POST["code"] == "もしもし,私は秋谷") {
+    echo 'You are correct. <a href="moshimoshiwatashiwaakiya.html">Click here to continue.</a>'
+} else {
+    header("Location: https://jppuzzlechallenge.github.io/japanwelcomesyou.html");
+}
+?>
+
+
+</body>
+</html>
